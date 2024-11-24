6bf996ef4e3913688547d26587dad6bd10414b25
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-24 20:06:18 -0300

    Alteração para PHP

--- docs/login/paginalogin.php
+++ docs/login/paginalogin.php
@@ -3,32 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
     <div class="wrapper">
-        <form action="">
+        <form action="process_login.php" method="POST">
             <h1>Kelma</h1>
             <div class="input-box">
-                <input type="text" placeholder="e-mail">
-                <i class='bx bx-user' ></i>
+                <input type="email" name="email" placeholder="e-mail" required>
+                <i class='bx bx-user'></i>
             </div>
             <div class="input-box">
-                <input type="password" placeholder="senha">
-                <i class='bx bxs-lock-alt' ></i>
+                <input type="password" name="password" placeholder="senha" required>
+                <i class='bx bxs-lock-alt'></i>
             </div>
             <div class="remember-forgot">
-                <label><input type="checkbox">lembrar</label>
+                <label><input type="checkbox" name="remember">lembrar</label>
                 <a href="#">Esqueceu a senha?</a>
             </div>
-            
             <div class="register-link">
                 <br>
-                <a href="/inicio/inicio.html" style="padding: 10px 80px; background-color: white; border-radius: 10px; font-size: 16px; text-decoration: none;">login</a>
+                <button type="submit" class="botao-login">Entrar</button>
                 <br>
-                <p style="margin-top: 15px;">Não tem uma conta? <a href="/cadastro/cadastro.html"><strong>cadastre-se</strong></a></p>
+                <p style="margin-top: 15px;">Não tem uma conta? <a href="/docs/cadastro/cadastro.html"><strong>cadastre-se</strong></a></p>
             </div>
         </form>
     </div>
