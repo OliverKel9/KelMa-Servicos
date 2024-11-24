fc7d40bf0d115fdff0cb229e8caab56bb83c82ff
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-24 20:14:17 -0300

    Adicionando PHP

---
+++ docs/inclludes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'seu_banco';
+$user = 'seu_usuario';
+$password = 'sua_senha';
+
+try {
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+}
+?>
