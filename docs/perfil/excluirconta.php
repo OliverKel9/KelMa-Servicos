3a0752adad804c3e4eea2401b64e41f4e5b12968
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-26 18:33:21 -0300

    log

--- docs/perfil/excluirconta.php
+++ docs/perfil/excluirconta.php
@@ -4,6 +4,8 @@ include_once 'docs/includes/db.php';
 
 // Iniciar a sessão para obter o ID do usuário logado
 session_start();
+var_dump($_SESSION); // Depuração para ver o que está na sessão
+exit;
 
 // Verificar se o usuário está logado
 if (!isset($_SESSION['user_id'])) {
