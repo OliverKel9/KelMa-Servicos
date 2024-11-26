5a8cc2c26e7bb17dbdc9aec0f0104b0296d6e489
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-26 16:56:23 -0300

    adicionando página inicio

---
+++ docs/inicio/inicio.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="KelMa">
+    <title>Projeto Faculdade</title>
+    <link rel="stylesheet" href="/docs/Inicio/inicio.css">
+    <link rel="stylesheet" href="/docs/Inicio/responcivo-I.css">
+</head>
+
+</body>
+
+    <nav class="navbar">
+        <div class="logo">
+            <a href="/docs/Inicio/inicio.html">KelMa</a>
+        </div>
+        <div class="Menu">
+            <a href="/docs/Inicio/inicio.html">Inicio</a>
+            <ul class="nav-list hidden">
+                <li><a href="/">Menu</a>
+                    <ul class="sub-menu">
+                        <li><a href="/">Perfil</a></li>
+                        <li><a href="/">Contratos</a></li>
+                        <li><a href="/">Sair</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        </div>
+    
+    </nav>
+
+    <header class="header">
+        <div class="headline">
+            <h2 style="letter-spacing: 5px;">Os melhores prestadores de</h2>
+            <h2 style="font-size: 80px;">Serviços</h2>
+            <p style="font-size: 20px;">KelMa - Foi criado para facilitar a vida de quem busca profissionais para determinado serviço. Aqui, você pode encontrar especialistas qualificados para atender às suas necessidades. Tudo de forma rápida, prática e confiável!</p>
+
+        </div>
+
+
+        <div class="img-headline">
+            <img style="max-width: 70%;" src="/docs/img/Ferramentas.png.png">
+        </div>
+
+    </header>
+    <section>
+
+    
+        <h2> Qual serviço procura??</h2>
+        <br>
+
+        <div class="Servicos">
+            <a href="/docs/serviços/serviços.html">
+            <div class="card">
+                <img src="/docs/img/Martelo.png">
+                <div class="card-text">
+                <h3>Marceneiro</h3>
+                <p> Operário industrial que trabalha com madeira em tábua, na fabricação de móveis e certos objetos. Em decoração de interiores, e que, eventualmente, também. Domina técnicas de entalhe, embutidos etc.</p>
+            </div>
+            </div>
+            <div class="card">
+                <div class="card-text">
+                    <img src="/docs/img/Pincel.png">
+                    <h3>Pintor</h3>
+                    <p>Profissional especializado na aplicação de tintas, vernizes e outros revestimentos em superfícies diversas, como paredes, tetos e estruturas metálicas.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-text">
+                    <img src="/docs/img/chave.png.png">
+                    <h3>Eletricista</h3>
+                    <p>Proficional especialista em instalações elétricas residenciais e industriais. Eletricistas podem ser empregados em instalação de novas componentes elétricas ou em manutenção e reparação de infra-estruturas.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-text">
+                    <img src="/docs/img/Chave-ingkesa.png.png">
+                    <h3>Encanador</h3>
+                    <p>Proficional responsável pelo assentamento dos canos de distribuição de água e de gás, assim como o de filtros, louças sanitárias.</p>
+                </div>
+
+            </div>
+            
+        </div>
+
+    </section>
+
+    <footer>
+        <div class="logo-rodape">
+        <h1 class="agencia">KelMa</h1>
+        <p style="color: white;">Slogam da empresa</p>
+        </div>
+    </footer>
+
+    <script src="mobile-navbar.js"></script>
+
+</body>
+
+
+
+</html>
