4dea89b31575aa538b30ac323993352ff87f7886
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-26 17:43:02 -0300

    test

--- docs/perfil/perfil.php
+++ docs/perfil/perfil.php
@@ -1,63 +1,68 @@
-
 <!DOCTYPE html>
-<html lang="br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>pagina de perfil</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <title>Cadastro para Cliente</title>
+    <link rel="stylesheet" href="cadastro.css">
 </head>
+
 <body>
-  <header>
-    <nav class="navbar">
-      <div class="logo">
-          <a href="/docs/index.html">KelMa</a></h1>
-      </div>
-      <div class="Menu">
-          <a href="/inicio/inicio.html">Inicio</a>
-      </div>
-  
-  </nav>
-  </header>
-  <main>
-    <div class="card">
-      <div class="left-container">
-        <img src="fotomatheus.jpg" alt="Profile Image">
-        <h2 class="gradienttext">John Doe</h2>
-        <h1>Prestador de serviço</h1>
-      </div>
-      <div class="right-container">
-        <h3 class="gradienttext">Profile Details</h3>
-        <table>
-            <tr>
-              <h1>Kelma</h1>
-                <td>Nome :</td>
-                <td>Matheus Lacerda</td>
-              </tr>
-          <tr>
-            <td>idade :</td>
-            <td>25</td>
-          </tr>
-          <tr>
-            <td>Mobile :</td>
-            <td>+91 40028922</td>
-          </tr>
-          <tr>
-            <td>Email :</td>
-            <td>matheus.lacerda@kelma.com</td>
-          </tr>
-          <tr>
-            <td>cidade :</td>
-            <td>Campinas, SP</td>
-          </tr>
-          <tr>
-            <td>sobre mim :</td>
-            <td>Trabalho como pintor a 10 anos </td>
-          </tr>
-        </table>
-      </div>
-    </div>
-  </main>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="#">Kelma</a>
+            </div>
+            <div class="Menu">
+                <a href="/docs/index.php">Início</a>
+                <a id="botao" href="/docs/login/paginalogin.php">Entrar</a>
+            </div>
+        </nav>  
+    </header>
+
+    <main>
+        <section class="box">
+
+            <div class="h2_div"><h2>Cadastro</h2></div>
+            <br>
+            <form action="processarcadastro.php" method="post">
+                <label for="nome_completo">Nome Completo:</label>
+                <input type="text" id="nome_completo" name="nome_completo" placeholder="Nome"required>
+                <br><br><br>
+
+
+                <label for="idade">Idade:</label>
+                <input type="number" id="idade" name="idade" min="18" max="100" placeholder="Idade" required>
+                <br><br><br>
+
+
+                <label for="telefone">Telefone:</label>
+                <input type="number" id="telefone" name="telefone" placeholder="xx-9-xxxx-xxxx"
+                pattern="[0-9]{2}[9]{1}[0-9]{4}[0-9]{4}" required>
+                <br><br><br>
+
+                <label for="cpf">CPF:</label>
+                <input type="number" id= "cpf" name="cpf" placeholder="xxx-xxx-xxx-xx"
+                pattern="[0-9]{3}[0-9]{3}[0-9]{3}[0-9]{2}" required>
+                <br><br><br>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="exemplo@hotmail.com" required>
+                <br><br><br>
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" minlength="7" placeholder="Senha"required>
+                <br><br><br>
+                <br>
+
+                <button type="submit" style="padding: 10px 80px; background-color: white; border-radius: 10px; font-size: 16px;">Cadastrar</button>
+                
+            </form>
+
+        </section>
+    </main>   
+
 </body>
-</html>
