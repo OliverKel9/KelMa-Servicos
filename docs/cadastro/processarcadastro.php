8394624a0a8df23f60561c9c32911ead6814ee49
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-26 16:34:30 -0300

    correção

--- docs/cadastro/processarcadastro.php
+++ docs/cadastro/processarcadastro.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Captura os dados do formulário
     $nome_completo = filter_input(INPUT_POST, 'nome_completo', FILTER_SANITIZE_SPECIAL_CHARS);
     $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
-    $telefone = filter_input(INPUT_POST, 'tel', FILTER_SANITIZE_SPECIAL_CHARS);
+    $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_SPECIAL_CHARS);
     $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_SPECIAL_CHARS);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT); // Criptografa a senha
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Executa a query com os dados
         $stmt->execute([
-            ':nome_completo' => $nome,
+            ':nome_completo' => $nome_completo,
             ':idade' => $idade,
             ':telefone' => $telefone,
             ':cpf' => $cpf,
