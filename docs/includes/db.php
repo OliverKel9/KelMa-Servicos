25bff4d6df56712a77b4a219cd6c1555f4f31868
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-26 16:39:20 -0300

    test

--- docs/includes/db.php
+++ docs/includes/db.php
@@ -5,8 +5,8 @@ $user = 'postgres';
 $password = 'kel123!';
 
 try {
-    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão com o banco de dados: " . $e->getMessage());
 }
