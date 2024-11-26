f602efcc377a4a7ea4d1f2c99e386266ada6c9b5
OliverKel9 <161046537+OliverKel9@users.noreply.github.com>
2024-11-26 18:27:35 -0300

    log

--- docs/perfil/excluirconta.php
+++ docs/perfil/excluirconta.php
@@ -24,7 +24,7 @@ if (isset($_POST['deletar']) && isset($_POST['user_id'])) {
         // Destruir a sessão e redirecionar para a página de login
         session_unset();
         session_destroy();
-        header("Location: /docs/login/index.php?status=conta_excluida");
+        header("Location: /docs/index.php?status=conta_excluida");
         exit;
     } else {
         echo "Erro ao excluir a conta.";
